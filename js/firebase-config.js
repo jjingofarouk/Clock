8307312313e5ba21e55dac7fd96c55204894f4da
jjingofarouk <jjingofaroukk@gmail.com>
2025-01-19 19:27:46 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,15 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  // Replace with your Firebase config
-  apiKey: "your-api-key",
-  authDomain: "your-domain.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-bucket.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBs_owPRHHRy2fPlbT0nT8zRyOWt1_VF5k",
+    authDomain: "kindness-44d5d.firebaseapp.com",
+    projectId: "kindness-44d5d",
+    storageBucket: "kindness-44d5d.firebasestorage.app",
+    messagingSenderId: "664744353279",
+    appId: "1:664744353279:web:c17e3c7ac6e4106b931bca",
+    measurementId: "G-TSSKVLD23F"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
